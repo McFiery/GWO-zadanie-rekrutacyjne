fdace03908a03f081040c91ae55c48e4b9bfd76d
Oskar Piotrowski <o.piotrowski@lixium.pl>
2021-12-04 01:26:39 +0100

    Add Item and Quantity too low Exception

---
+++ src/Cart/Exception/QuantityTooLowException.php
@@ -0,0 +1,11 @@
+<?php
+
+  namespace Recruitment\Cart\Exception;
+
+  use Exception;
+  use Throwable;
+
+class QuantityTooLowException extends Exception implements Throwable
+{
+
+}
---
+++ src/Cart/Item.php
@@ -0,0 +1,62 @@
+<?php
+
+  namespace Recruitment\Cart;
+
+use InvalidArgumentException;
+use Recruitment\Cart\Exception\QuantityTooLowException;
+use Recruitment\Entity\Product;
+
+class Item
+{
+    private $product;
+    private $quantity;
+
+  /**
+   * @param Product $product
+   * @param int $quantity
+   */
+    public function __construct(Product $product, int $quantity)
+    {
+        $this->product = $product;
+        if ($this->product->getMinimumQuantity() <= $quantity) {
+            $this->quantity = $quantity;
+        } else {
+            throw new InvalidArgumentException();
+        }
+    }
+
+    public function getProduct():Product
+    {
+        return $this->product;
+    }
+
+  /**
+   * @throws QuantityTooLowException
+   */
+    public function setQuantity(int $quantity):Product
+    {
+        $ProductMinimumQuantity = $this->product->getMinimumQuantity();
+        if ($quantity >= $ProductMinimumQuantity) {
+            $this->quantity = $quantity;
+        } else {
+            throw new QuantityTooLowException('The quantity cannot be less than ' . $ProductMinimumQuantity);
+        }
+        return $this->product;
+    }
+
+    public function getQuantity():int
+    {
+        return $this->quantity;
+    }
+
+    public function getTotalPrice():int
+    {
+        return $this->calculateTotalPrice();
+    }
+
+    public function calculateTotalPrice():int
+    {
+        $productPrice = $this->product->getUnitPrice();
+        return $productPrice * $this->quantity;
+    }
+}
