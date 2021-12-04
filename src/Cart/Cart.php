4af284c7d0e9636a9fde921227f60dd15e543cf6
Oskar Piotrowski <o.piotrowski@lixium.pl>
2021-12-04 01:27:53 +0100

    Create Cart

---
+++ src/Cart/Cart.php
@@ -0,0 +1,14 @@
+<?php
+
+  namespace Recruitment\Cart;
+
+use Recruitment\Entity\Product;
+
+class Cart
+{
+    private $items;
+
+    public function addProduct(Product $product, int $quantity)
+    {
+    }
+}
