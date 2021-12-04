78fd658eba54f4522b9f56a9edff30af4faab1a8
Oskar Piotrowski <o.piotrowski@lixium.pl>
2021-12-04 16:51:58 +0100

    Added Order.php and logic for Order

--- src/Entity/Order.php
+++ src/Entity/Order.php
@@ -2,7 +2,35 @@
 
   namespace Recruitment\Entity;
 
-  class Order
-  {
+use Recruitment\Cart\Cart;
 
-  }
+class Order
+{
+    private $orderId;
+    private $orderItems = [];
+    private $totalPrice;
+
+    public function __construct(int $orderId, Cart $cart)
+    {
+        $this->orderId = $orderId;
+        $this->orderItems = $cart->getItems();
+        $this->totalPrice = $cart->getTotalPrice();
+    }
+
+    public function getDataForView():array
+    {
+        $view = [];
+
+        $view['id'] = $this->orderId;
+        foreach ($this->orderItems as $item) {
+            $viewItem['id'] = $item->getProduct()->getId();
+            $viewItem['quantity'] = $item->getQuantity();
+            $viewItem['total_price'] = $item->getTotalPrice();
+
+            $view['items'][] = $viewItem;
+        }
+        $view['total_price'] = $this->totalPrice;
+
+        return $view;
+    }
+}
